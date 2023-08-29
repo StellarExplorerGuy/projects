047383c5c5539d82648bb56dee253b9464a3ddf0
Andrej Strela <andrej.strela@ibm.com>
2023-08-29 09:03:55 +0300

    improvements

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '@fontsource/inter';
 
 import App from './App'
 
 const rootElement = document.createElement('div')
-rootElement.id = 'root'
+rootElement.id = '3e23e23e-root'
 document.body.appendChild(rootElement)
 
 const root = ReactDOM.createRoot(rootElement)
