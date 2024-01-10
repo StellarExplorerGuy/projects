d8bd282924fba3cb35f117723fdb062ba73d2a39
Andrej Strela <andrej.strela@ibm.com>
2024-01-10 18:50:03 +0200

    fix: getHeader

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -7,116 +7,116 @@ import { getService } from './utils/data'
 
 const FASTER_PR_SCRIPT_ID = 'faster-pr-1we4'
 
-// const getHeader = (service: SERVICE) => {
-//   let headerElement
-//   if (service === SERVICE.GITHUB) {
-//     headerElement = document.getElementsByClassName('gh-header-title')
-//     headerElement = headerElement?.length > 0 ? headerElement[0] : null
-//   } else if (service === SERVICE.GITLAB) {
-//     headerElement = document.querySelector('[data-testid="issue-title"]')
-//     //enable for merge_requests
-//     if (!headerElement) {
-//       headerElement = document.querySelector('[data-testid="title-content"]')
-//     }
-//   } else if (service === SERVICE.TRELLO) {
-//     headerElement = document.getElementsByClassName('window-header js-card-detail-header')
-//     headerElement = headerElement?.length > 0 ? headerElement[0] : null
-//   } else if (service === SERVICE.JIRA_DEFAULT) {
-//     headerElement = document.getElementById('jira-issue-header')
-//   } else if (service === SERVICE.JIRA_COMPANY_1) {
-//     headerElement = document.getElementsByClassName('issue-header-content')
-//     headerElement = headerElement?.length > 0 ? headerElement[0] : null
-//   } else if (service === SERVICE.JIRA_COMPANY_2) {
-//     headerElement = document.getElementsByClassName('issue-header-content')
-//     headerElement = headerElement?.length > 0 ? headerElement[0] : null
-//   } else if (service === SERVICE.MONDAY_DEFAULT) {
-//     headerElement = document.querySelector('.pulse_title')
-//   }
-//   return headerElement
-// }
+const getHeader = (service: SERVICE) => {
+  let headerElement
+  if (service === SERVICE.GITHUB) {
+    headerElement = document.getElementsByClassName('gh-header-title')
+    headerElement = headerElement?.length > 0 ? headerElement[0] : null
+  } else if (service === SERVICE.GITLAB) {
+    headerElement = document.querySelector('[data-testid="issue-title"]')
+    //enable for merge_requests
+    if (!headerElement) {
+      headerElement = document.querySelector('[data-testid="title-content"]')
+    }
+  } else if (service === SERVICE.TRELLO) {
+    headerElement = document.getElementsByClassName('window-header js-card-detail-header')
+    headerElement = headerElement?.length > 0 ? headerElement[0] : null
+  } else if (service === SERVICE.JIRA_DEFAULT) {
+    headerElement = document.getElementById('jira-issue-header')
+  } else if (service === SERVICE.JIRA_COMPANY_1) {
+    headerElement = document.getElementsByClassName('issue-header-content')
+    headerElement = headerElement?.length > 0 ? headerElement[0] : null
+  } else if (service === SERVICE.JIRA_COMPANY_2) {
+    headerElement = document.getElementsByClassName('issue-header-content')
+    headerElement = headerElement?.length > 0 ? headerElement[0] : null
+  } else if (service === SERVICE.MONDAY_DEFAULT) {
+    headerElement = document.querySelector('.pulse_title')
+  }
+  return headerElement
+}
 
-// function process(headerElement: Element, currentService: SERVICE) {
-//   const fasterPRScripts = document.querySelectorAll(`#${FASTER_PR_SCRIPT_ID}`)
-//   fasterPRScripts.forEach((fasterPRScript) => {
-//     fasterPRScript.remove()
-//   })
-//   if (headerElement) {
-//     const rootElement = document.createElement('div')
-//     rootElement.id = FASTER_PR_SCRIPT_ID
-//     Object.assign(rootElement.style, {
-//       display: 'grid',
-//       justifyItems: 'start',
-//     })
+function process(headerElement: Element, currentService: SERVICE) {
+  const fasterPRScripts = document.querySelectorAll(`#${FASTER_PR_SCRIPT_ID}`)
+  fasterPRScripts.forEach((fasterPRScript) => {
+    fasterPRScript.remove()
+  })
+  if (headerElement) {
+    const rootElement = document.createElement('div')
+    rootElement.id = FASTER_PR_SCRIPT_ID
+    Object.assign(rootElement.style, {
+      display: 'grid',
+      justifyItems: 'start',
+    })
 
-//     if (currentService === SERVICE.TRELLO) {
-//       Object.assign(rootElement.style, {
-//         left: '-56px',
-//         position: 'relative',
-//       })
-//     }
+    if (currentService === SERVICE.TRELLO) {
+      Object.assign(rootElement.style, {
+        left: '-56px',
+        position: 'relative',
+      })
+    }
 
-//     headerElement.appendChild(rootElement)
-//     createRoot(rootElement).render(
-//       <React.StrictMode>
-//         <App />
-//       </React.StrictMode>,
-//     )
-//   }
-// }
+    headerElement.appendChild(rootElement)
+    createRoot(rootElement).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    )
+  }
+}
 
-// //PROD
-// let debounceTimeout: number | undefined
+//PROD
+let debounceTimeout: number | undefined
 
-// function onInitAvailable() {
-//   clearTimeout(debounceTimeout)
-//   const url = window.location.href
-//   const currentService = getService()
+function onInitAvailable() {
+  clearTimeout(debounceTimeout)
+  const url = window.location.href
+  const currentService = getService()
 
-//   if (url) {
-//     try {
-//       const headerElement = getHeader(currentService)
-//       debounceTimeout = setTimeout(() => {
-//         if (currentService && headerElement) {
-//           let isStyleLoaded = false
-//           const buttonRendered = document.getElementById('wjdkwed1')
-//           if (buttonRendered) {
-//             // resolve the issue if style isn't loaded
-//             const selectedButton = document.querySelector('button.Mui-selected')
-//             if (selectedButton) {
-//               isStyleLoaded = !!getComputedStyle(selectedButton).color
-//             }
-//             const hasClickEvent = buttonRendered.onclick !== null && isStyleLoaded
-//             if (hasClickEvent) {
-//               // observerInit.disconnect()
-//             } else {
-//               process(headerElement, currentService)
-//             }
-//           } else {
-//             process(headerElement, currentService)
-//           }
-//         }
-//       }, 200)
-//     } catch (error) {
-//       console.error('[error]', error)
-//     }
-//   }
-// }
+  if (url) {
+    try {
+      const headerElement = getHeader(currentService)
+      debounceTimeout = setTimeout(() => {
+        if (currentService && headerElement) {
+          let isStyleLoaded = false
+          const buttonRendered = document.getElementById('wjdkwed1')
+          if (buttonRendered) {
+            // resolve the issue if style isn't loaded
+            const selectedButton = document.querySelector('button.Mui-selected')
+            if (selectedButton) {
+              isStyleLoaded = !!getComputedStyle(selectedButton).color
+            }
+            const hasClickEvent = buttonRendered.onclick !== null && isStyleLoaded
+            if (hasClickEvent) {
+              // observerInit.disconnect()
+            } else {
+              process(headerElement, currentService)
+            }
+          } else {
+            process(headerElement, currentService)
+          }
+        }
+      }, 200)
+    } catch (error) {
+      console.error('[error]', error)
+    }
+  }
+}
 
-// const observerInit = new MutationObserver(onInitAvailable)
-// observerInit.observe(document.body, {
-//   childList: true,
-//   subtree: true,
-// })
+const observerInit = new MutationObserver(onInitAvailable)
+observerInit.observe(document.body, {
+  childList: true,
+  subtree: true,
+})
 
 ///////////
 // DEV
-const rootElement = document.createElement('div')
-rootElement.id = FASTER_PR_SCRIPT_ID
-document.body.appendChild(rootElement)
+// const rootElement = document.createElement('div')
+// rootElement.id = FASTER_PR_SCRIPT_ID
+// document.body.appendChild(rootElement)
 
-const root = createRoot(rootElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// const root = createRoot(rootElement)
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
